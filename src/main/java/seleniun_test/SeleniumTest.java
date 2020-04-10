b2e421d3e173feac32edd3219a7a9a00571b12ec
srane <sachin.rane@aciworldwide.com>
2020-04-10 10:30:26 +0000

    added chrome support

--- src/main/java/seleniun_test/SeleniumTest.java
+++ src/main/java/seleniun_test/SeleniumTest.java
@@ -7,7 +7,7 @@ import java.util.TimerTask;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,6 +15,8 @@ public class SeleniumTest {
 	public static final long DELAY  = 1860000;
 	private static final String  URL = "http://mycutebaby.in/contest/participant/?n=5e8aa945525c4&utm_source=wsapp_share&utm_campaign=April_2020&utm_medium=shared&utm_term=wsapp_shared_5e8aa945525c4&utm_content=participant";
 	private static final String  FIREFOX_DRIVER = "C:\\Users\\sachin\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe";
+	private static final String  FIREFOX_DRIVER_UNIX = "/usr/bin/geckodriver";
+	private static final String  CHROME_DRIVER_UNIX = "/usr/bin/chromedriver";
 	private static int count = 0;
 			
 	public static void main(String[] args) throws InterruptedException {
@@ -44,11 +46,11 @@ public class SeleniumTest {
 
 	private static void runClickJob() throws InterruptedException {
 		//System.setProperty("webdriver.ie.driver", "C:\\Users\\sachin\\Downloads\\IEDriverServer_x64_3.4.0\\IEDriverServer.exe");
-		System.setProperty("webdriver.gecko.driver",FIREFOX_DRIVER);
+		System.setProperty("webdriver.gecko.driver",CHROME_DRIVER_UNIX);
 		
-		WebDriver driver=new FirefoxDriver();
+		WebDriver driver=new ChromeDriver();
 		driver.get(URL);
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
 		//WebElement elementName = driver.findElement(By.className("vote-btn"));
 		//driver.findElement(By.id("v")).sendKeys("dgfdff");;
 		
